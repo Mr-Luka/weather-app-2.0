f5c7d97aa957dcfee5f69653de97044d93d800d3
Luka <luka.luka.luka93@gmail.com>
2024-07-01 14:41:06 -0700

    selected all the objects needed towork with in javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+const app = document.querySelector(".weather-app");
+const temp = document.querySelector(".temp");
+const cityName = document.querySelector(".name");
+const time = document.querySelector(".time");
+const date = document.querySelector(".date");
+const weatherIcon = document.querySelector(".icon");
+const weatherCondition = document.querySelector(".condition");
+const form = document.querySelector("#localInput");
+const input = document.querySelector(".search");
+const btn = document.querySelector(".submit");
+const cities = document.querySelectorAll(".city");
+const cloudy = document.querySelector(".cloud");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+
+
+
+
