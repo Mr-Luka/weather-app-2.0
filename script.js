f5e6f02ca69bb139bd082ead9f2ba0a0fe608730
Luka <luka.luka.luka93@gmail.com>
2024-07-01 15:43:56 -0700

    made a function for the dayOf the week

--- script.js
+++ script.js
@@ -36,3 +36,16 @@ form.addEventListener("submit", e=> {
     e.preventDefault();
     
 })
+function dayOfTheWeek (day, month, year) {
+    const weekday = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    return weekday[new Date(`${day}/${month}/${year}`).getDay()]
+}
+
